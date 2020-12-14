2a026cc30d3146b9a8253d26970731b086cf8884
yenanw <yenanw@yn-wg.com>
2020-12-14 19:38:23 +0100

    clean up day1 code

---
+++ day1/Day1.java
@@ -0,0 +1,29 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Day1 {
+    public static void main(String args[]) {
+        try {
+            String inputFile = args[0];
+            int[] nums = Files.lines(Paths.get(inputFile))
+                        .mapToInt(i->Integer.parseInt(i))
+                        .toArray();
+
+            //----------------PART 1----------------
+            int[] pair = NSum.findPair(nums, 2020);
+            System.out.println("The two numbers that sum to 2020 is: " +
+                                pair[0] + " and " + pair[1]);
+            System.out.println("The product is " + pair[0]*pair[1]);
+
+            //----------------PART 2----------------
+            int[] triple = NSum.findTriple(nums, 2020);
+            System.out.println("The three numbers that sum to 2020 is: " +
+                                triple[0] + ", " + triple[1] + " and " + triple[2]);
+            System.out.println("The product is " + triple[0]*triple[1]*triple[2]);
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ day1/NSum.java
@@ -0,0 +1,61 @@
+/**
+ * A simple helper class for solving the 2-sum/3-sum problem
+ *
+ * @author       yenanw
+ * @version      1.0
+ * @see also     Day1
+ */
+public class NSum {
+
+     /**
+      * A brute-force algorithm for finding the first pair in the array which
+      * sums to a given number
+      *
+      * @param input The int array to search through
+      * @param sum   The number being tested
+      * @return An empty array if no pairs can be found, else an int array that
+      *         consists of the 2 integers that sums to the given numbers
+      */
+     static public int[] findPair(int input[], int sum) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[i] + input[j] == sum) {
+                    return new int[] {
+                        input[i], input[j]
+                    };
+                }
+            }
+        }
+
+        return new int[0];
+    }
+
+    /**
+      * A brute-force algorithm for finding the first triple in the array which 
+      * sums to a given number
+      *
+      * @param input The int array to search through
+      * @param sum   The number being tested
+      * @return An empty array if no triples can be found, else an int array
+      *         that consists of the 3 integers that sums to the given numbers
+      */
+    static public int[] findTriple(int input[], int sum) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                for (int o = j + 1; o < input.length; o++) {
+                    if (input[i] + input[j] + input[o] == sum) {
+                        return new int[] {
+                            input[i], input[j], input[o]
+                        };
+                    }
+                }
+            }
+        }
+
+        return new int[0];
+    }
+
+    private NSum() {
+        // don't instantiate me!
+    }
+}
