d66d984f3cbad406dc40ffb47346806929b25158
yenanw <yenanw@yn-wg.com>
2020-12-20 21:09:45 +0100

    day20, added the comments to make the code look pro

--- day20/TileManager.java
+++ day20/TileManager.java
@@ -3,24 +3,49 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Class created to solve the day20 challenge
+ *
+ * @author       yenanw
+ * @version      1.0
+ * @see also     Day20, Tile
+ */
 public class TileManager {
     private Tile[] tiles;
     private int size;
 
+    /**
+     * Constructs the class with the specified image tiles
+     * 
+     * @param tiles All of the tiles from the input, note that in order to
+     *              solve the puzzle, the length of the tiles must be a squared
+     *              number so that the final image will form a square
+     */
     public TileManager(Tile[] tiles) {
         this.tiles = tiles;
         this.size = (int)Math.sqrt(tiles.length);
     }
 
+    /**
+     * Rearrange all tiles so that they are all connected
+     */
     public void arrange() {
         Tile[] corners = findCorners();
         arrange(corners[0], new HashSet<>());
     }
 
+    /**
+     * A scuffed method to find the corners from the given tiles, I don't know
+     * why it works, but it does
+     * 
+     * @return The corners of the tiles given from the start
+     */
     public Tile[] findCorners() {
         List<Tile> corners = new ArrayList<>();
         for (int i = 0; i < tiles.length; i++) {
             Tile t1 = tiles[i];
+            // for each tile in the array, go through all other tiles and see
+            // if any of them matches a side
             for (int j = 0; j < tiles.length; j++) {
                 if (i == j)
                     continue;
@@ -37,7 +62,7 @@ public class TileManager {
                 if (t1.isReady())
                     break;
             }
-
+            // if only two sides are matched, then this tile must be a corner,
             if (t1.isCorner())
                 corners.add(t1);
 
@@ -47,30 +72,48 @@ public class TileManager {
         return corners.toArray(Tile[]::new);
     }
 
+    /**
+     * Starting from the top left corner, recursively go through the right and
+     * bottom tiles to add their respective tiles to an int matrix, and
+     * contruct the image tile from the matrix
+     * 
+     * @return A tile of the actual image
+     */
     public Tile concatTiles() {
         Tile topLeft = null;
+        // finds the top left corner
         for (Tile tile : tiles) {
             if (tile.top == null && tile.left == null && tile.isCorner()) {
                 topLeft = tile;
                 break;
             }
         }
-
+        // calculates the side lengths, -2 because we need to strip the border
         int row = topLeft.getTile().length - 2;
         int col = topLeft.getTile()[0].length - 2;
         int[][] image = new int[size * row][size * col];
 
         concatTiles(topLeft, image, 0, 0, new HashSet<>());
+        // contructs the image tile, id is kina whatever
         return new Tile(image, -1);
     }
 
+    /**
+     * Goes through all possible arrangements of the image tile and tries to
+     * count the amount of times the pattern appears in teh tile
+     * 
+     * @param t       The tile to find the pattern from
+     * @param pattern THe int matrix representing the pattern
+     * @return The amount of times the pattern appears in the tile
+     */
     public int countMatchingPattern(Tile t, int[][] pattern) {
         int count = 0;
         List<int[][]> arrangements = t.getArrangements();
 
         int p = 0;
         int[][] tile = arrangements.get(p);
-
+        // observe this absolute beauty, surprised it doesn't take years to
+        // compute this loop 
         while (count == 0) {
             for (int i = 0; i < tile.length - pattern.length; i++) {
                 for (int j = 0; j < tile[0].length - pattern[0].length; j++) {
@@ -92,55 +135,31 @@ public class TileManager {
                         count++;
                 }
             }
-            
             tile = arrangements.get(p++);
-            if (p >= arrangements.size()) 
+            if (p >= arrangements.size())
                 break;
         }
         return count;
     }
 
-    public int countCorners() {
-        int count = 0;
-        for (Tile tile : tiles) {
-            if (tile.isCorner())
-                count++;
-        }
-        return count;
-    }
-
-    public int countReady() {
-        int count = 0;
-        for (Tile tile : tiles) {
-            if (tile.isReady())
-                count++;
-        }
-        return count;
-    }
-
-    public int countEdge() {
-        int count = 0;
-        for (Tile tile : tiles) {
-            if (tile.isEdge())
-                count++;
-        }
-        return count;
-    }
-
+    /**
+     * Simply call reset on all tiles to completely reset everything
+     */
     public void reset() {
         for (Tile tile : tiles) {
             tile.reset();
         }
     }
 
-     private void concatTiles(Tile current, int[][] imageTile,
-                                int row, int col, Set<Tile> visited) {
+    private void concatTiles(Tile current, int[][] imageTile,
+                             int row, int col, Set<Tile> visited) {
+        // it's possible to run into an already visited tile
         if (visited.contains(current))
             return;
 
         int[][] tile = current.stripBorder();
         visited.add(current);
-
+        // concatenate the tile at the specified indices
         for (int r = 0; r < tile.length; r++) {
             for (int c = 0; c < tile[0].length; c++) {
                 imageTile[row+r][col+c] = tile[r][c];
@@ -149,12 +168,12 @@ public class TileManager {
         
         int rowLength = current.getTile().length - 2;
         int colLength = current.getTile()[0].length - 2;
-
+        // as long as we are not at the right edge, concatenate the right tile
         if (current.right != null) {
             concatTiles(current.right, imageTile, 
                         row, col+colLength, visited);
         }
-
+        // as long as we are not at the bottom edge, concatenate the bottom tile
         if (current.bottom != null) {
             concatTiles(current.bottom, imageTile, 
                         row+rowLength, col, visited);
@@ -162,14 +181,15 @@ public class TileManager {
     }
 
     private void arrange(Tile t1, Set<Tile> visited) {
+        // note that the start tile should be one of corners
         if (visited.contains(t1))
             return;
         visited.add(t1);
 
+        // find all sides of the specified tile
         for (Tile t2 : tiles) {
             if (t1.equals(t2) || t1.isReady())
                 continue;
-
             if (t2.isFree()) {
                 for (int[][] tArr : t2.getArrangements()) {
                     t2.setTile(tArr);
@@ -180,22 +200,16 @@ public class TileManager {
                 matchTiles(t1, t2);
             }
         }
-
-        if (t1.top != null) {
+        // recursively go through all of the tiles' sides to find their
+        // matching sides untill we have visited all of the tiles
+        if (t1.top != null)
             arrange(t1.top, visited);
-        }
-
-        if (t1.bottom != null) {
+        if (t1.bottom != null)
             arrange(t1.bottom, visited);
-        }
-
-        if (t1.left != null) {
+        if (t1.left != null)
             arrange(t1.left, visited);
-        }
-
-        if (t1.right != null) {
+        if (t1.right != null)
             arrange(t1.right, visited);
-        }
     }
 
     private boolean matchTiles(Tile t1, Tile t2) {
