8627c47f8f9b0ed7440a832df966703a62a0bcb2
yenanw <yenanw@yn-wg.com>
2020-12-21 19:18:04 +0100

    added comments for day19

--- day19/MsgParser.java
+++ day19/MsgParser.java
@@ -1,19 +1,49 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A class responsible for parsing the rules described in the day19 challenge,
+ * despite being called a parser, all of the "real" logic actually lies in the
+ * Rule class
+ *
+ * @author       yenanw
+ * @version      1.0
+ * @see also     Day19, Rule
+ */
 public class MsgParser {
     private Rule[] rules;
     private String[] messages;
 
+    /**
+     * Constructs the parser class with a set of rules and a set of messages
+     * to be parsed
+     * 
+     * @param rules
+     * @param messages
+     */
     public MsgParser(Rule[] rules, String[] messages) {
         this.rules = rules;
         this.messages = messages;
     }
 
+    /**
+     * Tests if the specified message can be parsed by the rule specified using
+     * its id
+     * 
+     * @param ruleId The id of the rule to match
+     * @param msg    The message string to test if it passes the rule
+     * @return true if the message parses successfully, else false
+     */
     public boolean matches(int ruleId, String msg) {
         return getRule(ruleId).matches(msg);
     }
 
+    /**
+     * Retrieves all messages that can be parsed using the specified rule 
+     * 
+     * @param ruleId The id of the rule to match
+     * @return An array of message that is parsed successfully
+     */
     public String[] allMatches(int ruleId) {
         Rule rule = getRule(ruleId);
         List<String> match = new ArrayList<>();
@@ -25,6 +55,12 @@ public class MsgParser {
         return match.toArray(String[]::new);
     }
 
+    /**
+     * Finds the rule with an id matching the specified id
+     * 
+     * @param ruleId The id of a rule
+     * @return The rule with an id matching the specified id, else null
+     */
     public Rule getRule(int ruleId) {
         for (Rule rule : rules) {
             if (rule.getID() == ruleId)
--- day19/Rule.java
+++ day19/Rule.java
@@ -1,8 +1,16 @@
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * The rule datatype describe in the day19 challenge, since I'm too lazy to
+ * abstractify it, it will have every parameter specified in the challenge,
+ * naturally only some of them will be used by each rule
+ *
+ * @author       yenanw
+ * @version      1.0
+ * @see also     Day19, MsgParser
+ */
 public class Rule {
     private List<Rule> fst = null;
     private List<Rule> snd = null;
@@ -11,40 +19,86 @@ public class Rule {
     private int id;
     private RuleType ruleType;
 
+    /**
+     * A simple enum representing the three different cases a rule can be
+     */
     private enum RuleType {
         MATCH, CASCADE, CASCADE_OR
     }
 
+    /**
+     * Constructs the rules with an id and a string, the string is a pattern
+     * which this rule class needs to match
+     * 
+     * @param id    The id of this rule
+     * @param match The pattern to match
+     */
     public Rule(int id, String match) {
         this.id = id;
         this.match = match;
         this.ruleType = RuleType.MATCH;
     }
 
+    /**
+     * Constructs the rules with an id and a sequence of other rules, in order
+     * to pass the parsing, the message must satisfy all of the rules in the
+     * list sequentially
+     * 
+     * @param id       The id of this rule
+     * @param fstRules The sequence of rules
+     */
     public Rule(int id, List<Rule> fstRules) {
         this.id = id;
         this.fst = fstRules;
         this.ruleType = RuleType.CASCADE;
     }
 
+    /**
+     * Constructs the rules with an id and two sequences of otehr rules, in
+     * order to pass the parsing, the message must satify at least one of the
+     * sequenced rules
+     * 
+     * @param id       The id of this rule
+     * @param fstRules The first sequence of rules 
+     * @param sndRules The second sequence of rules
+     */
     public Rule(int id, List<Rule> fstRules, List<Rule> sndRules) {
         this(id, fstRules);
         this.snd = sndRules;
         this.ruleType = RuleType.CASCADE_OR;
     }
 
+    /**
+     * @return The id of this rule
+     */
     public int getID() {
         return id;
     }
 
+    /**
+     * Allows change to the first sequence of rules
+     * 
+     * @return The reference to the first ruleset list
+     */
     public List<Rule> getFirstRules() {
         return fst;
     }
 
+    /**
+     * Allows change to the second sequence of rules
+     * 
+     * @return The reference to the first ruleset list
+     */
     public List<Rule> getSecondRules() {
         return snd;
     }
 
+    /**
+     * Tests if the specified message matches the rule's specifications
+     * 
+     * @param msg The message to test
+     * @return true if this rule can parse the message, else false
+     */
     public boolean matches(String msg) {
         for (int last : matches(msg, 0)) {
             String parsed = msg.substring(0, last);
@@ -57,6 +111,7 @@ public class Rule {
 
     @Override
     public String toString() {
+        // this gives a really nice string representation of the object
         StringBuilder sb = new StringBuilder();
         sb.append(id).append(":");
         switch (ruleType) {
@@ -69,7 +124,7 @@ public class Rule {
                 }
                 break;
             case CASCADE_OR:
-                for (Rule r : fst) {
+                for (Rule r : fst) { // duplicate code anyone?
                     sb.append(" ").append(r.getID());
                 }
                 sb.append(" | ");
@@ -82,6 +137,7 @@ public class Rule {
     }
 
     private Set<Integer> matches(String msg, int start) {
+        // probably should generified it some more
         return switch (ruleType) {
             case MATCH -> isMatch(msg, start);
             case CASCADE -> passMatchTo(fst, msg, start);
@@ -91,30 +147,38 @@ public class Rule {
     }
 
     private Set<Integer> isMatch(String msg, int start) {
+        // the parsing fails if the pointer is out of bound
         if (start >= msg.length())
             return new HashSet<>();
-
+        
         int n = start + match.length();
         Set<Integer> next = new HashSet<>();
         next.add(n);
-
+        // only when there are no characters left in the string will the
+        // parsing be a valid one
         String m = msg.substring(start, n);
         if (match.equals(m))
             return next;
-
+        // simply return an empty set if the parsing fails
         return new HashSet<>();
     }
 
     private Set<Integer> passMatchTo(List<Rule> rules, String msg, int start) {
         Set<Integer> next = new HashSet<>();
+        // first case, it's here just to make the loop look cleaner
         next.addAll(rules.get(0).matches(msg, start)); 
+        
         for (int i = 1; i < rules.size(); i++) {
-            if (next.isEmpty())
+            if (next.isEmpty()) {
+                // exit condition, we don't want to waste time on a 
+                // failed parsing
                 return new HashSet<>();
-            else {
+            } else {
                 Set<Integer> nNext = new HashSet<>();
                 for (int n : next)
-                   nNext.addAll(rules.get(i).matches(msg, n));
+                    // explore all possiblities for the next rule
+                    nNext.addAll(rules.get(i).matches(msg, n));
+                // go to next rule and try all new possibilities again
                 next = nNext;
             }
         }
@@ -122,6 +186,8 @@ public class Rule {
     }
 
     private Set<Integer> passMatchToBoth(String msg, int start) {
+        // simply explores all possible outcome from both the first set of
+        // rules and the second set of rules
         Set<Integer> r1 = passMatchTo(fst, msg, start);
         Set<Integer> r2 = passMatchTo(snd, msg, start);
         r1.addAll(r2);
