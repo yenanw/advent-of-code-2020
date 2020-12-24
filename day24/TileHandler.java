21d2d799a253c27f98800f1b1aebb0f221922ac0
yenanw <yenanw@yn-wg.com>
2020-12-24 13:30:30 +0100

    day24 added comments

--- day24/TileHandler.java
+++ day24/TileHandler.java
@@ -5,19 +5,46 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
+/**
+ * Class representing the floor describe in the day24 challenge
+ *
+ * @author       yenanw
+ * @version      1.0
+ * @see also     Day24
+ */
 public class TileHandler {
     private Tile center;
     private Set<Tile> blackTiles;
-    private Tile[][] tileMatrix;
+    private Tile[][] floor;
     private String[] instructions;
 
+    /**
+     * Contructs this class given a list of instructions and a size of how big
+     * the floor is
+     * 
+     * @param instructions The list of instructions to fix the floor with
+     * @param size         The size of the floor
+     */
     public TileHandler(String[] instructions, int size) {
         this.blackTiles = new HashSet<>();
         this.instructions = instructions;
         // i'm just gonna say that i have ascended with this
-        this.tileMatrix = initTiles(size);
+        this.floor = initTiles(size);
     }
 
+    /**
+     * Runs through all the instructions to "renovate the floor" according to
+     * the day24 challenge
+     */
+    public void renovate() {
+        for (String ins : instructions) {
+            parseIns(ins);
+        }
+    }
+
+    /**
+     * Simulates what happens to the tiles after 1 day
+     */
     public void updateTiles() {
         Set<Tile> toBeFlipped = new HashSet<>();
         // the tiles that needs to be checked are the black tiles and all
@@ -47,12 +74,11 @@ public class TileHandler {
         }
     }
 
-    public void renovate() {
-        for (String ins : instructions) {
-            parseIns(ins);
-        }
-    }
-
+    /**
+     * Parses an instruction to flip a tile
+     * 
+     * @param ins The instruction the parse
+     */
     public void parseIns(String ins) {
         Tile tile = center;
         Queue<Character> chrs = strToQueue(ins);
@@ -71,11 +97,18 @@ public class TileHandler {
         flipTile(tile);
     }
 
+    /**
+     * Counts how many black tiles there are
+     * 
+     * @return The amount of all black tiles
+     */
     public int countBlackTiles() {
         return blackTiles.size();
     }
 
     private Queue<Character> strToQueue(String str) {
+        // convert the string to a queue of chars so that it becomes easier to
+        // process the directions
         Queue<Character> chrQueue = new LinkedList<>();
         for (char ch : str.toCharArray()) {
             chrQueue.add(ch);
@@ -84,6 +117,7 @@ public class TileHandler {
     }
     
     private Tile moveTile(Tile tile, String dir) {
+        // returns the tile it should be moving to
         return getNeighbors(tile).get(dir);
     }
 
@@ -96,22 +130,24 @@ public class TileHandler {
     }
 
     private Map<String,Tile> getNeighbors(Tile tile) {
+        // yeah, i dunno about hexagons, so i just harded-coded the neighbors,
         Map<String,Tile> neighbors = new HashMap<>();
         int r = tile.row, c = tile.col;
         
-        neighbors.put("e", tileMatrix[r][c+1]);
-        neighbors.put("w", tileMatrix[r][c-1]);
+        neighbors.put("e", floor[r][c+1]);
+        neighbors.put("w", floor[r][c-1]);
 
         if ((r & 1) == 0) {
-            neighbors.put("ne", tileMatrix[r-1][c]);
-            neighbors.put("se", tileMatrix[r+1][c]);
-            neighbors.put("nw", tileMatrix[r-1][c-1]);
-            neighbors.put("sw", tileMatrix[r+1][c-1]);
+            // when the row is even the
+            neighbors.put("ne", floor[r-1][c]);
+            neighbors.put("se", floor[r+1][c]);
+            neighbors.put("nw", floor[r-1][c-1]);
+            neighbors.put("sw", floor[r+1][c-1]);
         } else {
-            neighbors.put("ne", tileMatrix[r-1][c+1]);
-            neighbors.put("se", tileMatrix[r+1][c+1]);
-            neighbors.put("nw", tileMatrix[r-1][c]);
-            neighbors.put("sw", tileMatrix[r+1][c]);
+            neighbors.put("ne", floor[r-1][c+1]);
+            neighbors.put("se", floor[r+1][c+1]);
+            neighbors.put("nw", floor[r-1][c]);
+            neighbors.put("sw", floor[r+1][c]);
         }
 
         return neighbors;
@@ -128,6 +164,9 @@ public class TileHandler {
     }
 
     private Tile[][] initTiles(int size) {
+        // initialize the tile matrix given a size, so that the size of the
+        // floor becomes fixed, pretty scuffed and inefficient, but it becomes
+        // hella easier to code with
         Tile[][] m = new Tile[size][size];
         for (int r = 0; r < m.length; r++) {
             for (int c = 0; c < m[0].length; c++) {
@@ -138,6 +177,8 @@ public class TileHandler {
         return m;
     }
 
+    // the tile class representing a tile, in reality it's just a fancy point
+    // in a matrix
     private class Tile {        
         boolean isWhite = true;
         int row;
@@ -159,6 +200,7 @@ public class TileHandler {
         
         @Override
         public int hashCode() {
+            // scuffed hash function, good enough for this for now
             return row * col + col;
         }
     }
